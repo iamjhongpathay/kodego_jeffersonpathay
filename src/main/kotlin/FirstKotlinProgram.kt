f72daf30b985b6c18f64e44481e8b6610ff8d028
jhong pathay <jeffersonpathay@gmail.com>
2022-09-01 20:55:38 +0800

    first commit

---
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -0,0 +1,11 @@
+fun main(){
+    //Hello I am a comment.
+
+    /*
+    Author: Jefferson Pathay
+    Date created: Sept. 1, 2022
+    Filename: FirstKotlinProgram.kt
+     */
+
+    print("Hello World!")
+}
