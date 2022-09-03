09b0b9040160efb085bc181cc855f85aebc809fb
jhong pathay <jeffersonpathay@gmail.com>
2022-09-03 09:21:41 +0800

    3rd commit

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -6,9 +6,34 @@ fun main(){
     Date created: Sept. 1, 2022
     Filename: FirstKotlinProgram.kt
      */
+//    var firstName :String = "Jefferson"
+//    var age :Int = 25
+//    var grade :Double = 75.00
+//    var section :Char = 'A'
+//    var isEnrolled :Boolean = true
 
-    print("Hello World!")
-    print("Hello World!!")
-    print("Hello World!!")
+//    println("My name is $firstName")
+//    println("Age: $age")
+//    println("Grade: $grade")
+//    println("Section: $section")
+//    println("Enrolled $isEnrolled")
 
+//    print("Enter your name: ")
+//    var name :String = readln()
+//
+//    print("Enter your Age: ")
+//    var age :Int = readln().toInt()
+//
+//
+//    println("My name is $name")
+//    println("My age is $age")
+
+    print("Enter number 1:")
+    var number1 : Int = readln().toInt()
+    print("Enter number 2:")
+    var number2 : Int = readln().toInt()
+
+    var sum : Int = number1 + number2
+
+    println("The sum is $sum")
 }
