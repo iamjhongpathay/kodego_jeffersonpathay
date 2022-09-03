a60050724a0a420fa24102f999a9c66fa0f25abc
jhong pathay <jeffersonpathay@gmail.com>
2022-09-03 16:10:51 +0800

    My Kotlin Program 5

---
+++ src/main/kotlin/KotlinProgram5.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var itemNumber : Int = 3
+
+    when(itemNumber){
+        1 -> print("Clothes")
+        2 -> print("Food")
+        3 -> print("Toiletries")
+        else -> print("Invalid!")
+    }
+
+}
