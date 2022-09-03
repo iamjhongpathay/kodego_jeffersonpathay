5feb6e882655210ea4fd6766127671d2ce221f1b
jhong pathay <jeffersonpathay@gmail.com>
2022-09-03 15:44:48 +0800

    My Kotlin Program 4

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,45 @@
+fun main(){
+//    var number : Int = 9
+//    if(number < 10){
+//        println("Hello")
+//    }
+
+
+//    var grade : Int = 73
+//    if(grade >= 75){
+//        println("you passed!")
+//    }else{
+//        println("you failed.")
+//    }
+
+//    var grade : Int = 97
+//    if((grade >= 96) && (grade <= 100)){
+//        println("1.00")
+//    }else if ((grade >= 86) && (grade <= 95)){
+//        println("1.75")
+//    }else if((grade >= 76) && (grade <= 85)){
+//        println("2.25")
+//    }else if(grade <75){
+//        println("5.0")
+//    }else{
+//        println("Invalid grade!")
+//    }
+
+
+//    //range
+//    var idNumber : Int = 1
+//            //true            //true
+//    if((idNumber >= 1) && (idNumber <=3 )){
+//        println("valid id number")
+//    }
+//    println("Line Ends..")
+
+    var gender : String = "F"
+    if(gender == "M"){
+        println("Male")
+    }else if(gender == "F"){
+        println("Female")
+    }else{
+        println("Invalid!")
+    }
+}
