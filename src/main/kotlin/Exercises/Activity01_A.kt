5e2993a96880c666d47a24d38975ba322481cdec
jhong pathay <jeffersonpathay@gmail.com>
2022-09-03 12:27:51 +0800

    Practice Activity 01

---
+++ src/main/kotlin/Exercises/Activity01_A.kt
@@ -0,0 +1,9 @@
+fun main(){
+    //You are tasked to create a directory of the students taking this course.
+    // The data will be used to send updates regarding the classes.
+    // There is also a need to know the progress of the students during the progress of this course.
+
+    //List down all data that can be used to implement this. Use the proper data types.
+
+    var firstName : String
+}
---
+++ src/main/kotlin/Practice_Activities/PracticeActivity1.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    print("Enter your First name: ")
+    var firstname: String = readln()
+    print("Enter you Middle initial: ")
+    var middleInitial: String = readln()
+    print("Enter you Last Name: ")
+    var lastName: String = readln()
+
+    println()
+    println("Your name is $firstname " + "$middleInitial " + "$lastName")
+
+}
