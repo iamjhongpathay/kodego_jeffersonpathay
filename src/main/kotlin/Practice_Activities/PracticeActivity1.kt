6a133cba709e5040e4904da2d4da65d27430964d
jhong pathay <jeffersonpathay@gmail.com>
2022-09-03 18:34:47 +0800

    Practice Activity 01 3rdcom

--- src/main/kotlin/Practice_Activities/PracticeActivity1.kt
+++ src/main/kotlin/Practice_Activities/PracticeActivity1.kt
@@ -1,5 +1,17 @@
 fun main(){
 
+    /*
+    Practice Activity 1
+    Create a Kotlin program that will accept your first name, middle initial/name,
+    and last name then display them together in a single on the screen
+
+    Sample Output
+    Enter Your FirstName: John
+    Enter Your Middle Name/Initial: C.
+    Enter Your Last Name: Smith
+    Your Name is John C. Smith
+     */
+
     print("Enter your First name: ")
     var firstname: String = readln()
     print("Enter you Middle initial: ")
