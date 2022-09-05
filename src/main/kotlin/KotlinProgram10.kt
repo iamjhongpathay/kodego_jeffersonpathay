a103bf7c5eb605692ac177757c1f0010bef2dad6
jhong pathay <jeffersonpathay@gmail.com>
2022-09-05 20:22:01 +0800

    Kotlin Program 10 1stcom

---
+++ src/main/kotlin/KotlinProgram10.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var ctr1 : Int = 1
+    while(ctr1 <= 5){
+     var ctr2 : Int = 1
+        while(ctr2 <=5){
+            print("@")
+            ctr2++
+        }
+        println()
+        ctr1++
+    }
+}
+
+//output
+    //@@@@@
+    //@@@@@
+    //@@@@@
+    //@@@@@
+    //@@@@@
