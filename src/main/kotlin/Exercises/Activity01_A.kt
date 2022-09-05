f06948abd6c59738be1659251b005c877ff82527
jhong pathay <jeffersonpathay@gmail.com>
2022-09-05 16:27:24 +0800

    Activity01_C 2ndcom

--- src/main/kotlin/Exercises/Activity01_A.kt
+++ src/main/kotlin/Exercises/Activity01_A.kt
@@ -1,9 +1,73 @@
 fun main(){
-    //You are tasked to create a directory of the students taking this course.
-    // The data will be used to send updates regarding the classes.
-    // There is also a need to know the progress of the students during the progress of this course.
+    /*
+     You are tasked to create a directory of the students taking this course.
+     The data will be used to send updates regarding the classes.
+     There is also a need to know the progress of the students during the progress of this course.
 
-    //List down all data that can be used to implement this. Use the proper data types.
+    List down all data that can be used to implement this. Use the proper data types.
+    */
+    var student1Name : String = "AJ Vasquez"
+    var student1Age : Int = 26
+    var student1City :  String = "Quezon"
+    var student1PhoneNum: Long = "09991234512".toLong()
 
-    var firstName : String
+    var student2Name : String = "Aris Cruz"
+    var student2Age : Int = 29
+    var student2City :  String = "Pasay"
+    var student2PhoneNum: Long = "09991123456".toLong()
+
+    var student3Name : String = "Benedict van Velasco"
+    var student3Age : Int = 28
+    var student3City :  String = "Cavite"
+    var student3PhoneNum: Long = "09991234567".toLong()
+
+    var student4Name : String = "Jefferson Pathay"
+    var student4Age : Int = 25
+    var student4City :  String = "Las Piñas"
+    var student4PhoneNum: Long = "09994650716".toLong()
+
+    var student5Name : String = "Lesa Siroy"
+    var student5Age : Int = 27
+    var student5City :  String = "Cavite"
+    var student5PhoneNum: Long = "09991234567".toLong()
+
+
+    println("Welcome to KodeGo")
+    println("MD1P Students Directory")
+    println()
+
+    println("Student 1".uppercase())
+    println("Name: $student1Name")
+    println("Age: $student1Age")
+    println("City: $student1City")
+    println("Phone Number: $student1PhoneNum")
+    println()
+
+    println("Student 2".uppercase())
+    println("Name: $student2Name")
+    println("Age: $student2Age")
+    println("City: $student2City")
+    println("Phone Number: $student2PhoneNum")
+    println()
+
+    println("Student 3".uppercase())
+    println("Name: $student3Name")
+    println("Age: $student3Age")
+    println("City: $student3City")
+    println("Phone Number: $student3PhoneNum")
+    println()
+
+    println("Student 4".uppercase())
+    println("Name: $student4Name")
+    println("Age: $student4Age")
+    println("City: $student4City")
+    println("Phone Number: $student4PhoneNum")
+    println()
+
+    println("Student 5".uppercase())
+    println("Name: $student5Name")
+    println("Age: $student5Age")
+    println("City: $student5City")
+    println("Phone Number: $student5PhoneNum")
+
 }
