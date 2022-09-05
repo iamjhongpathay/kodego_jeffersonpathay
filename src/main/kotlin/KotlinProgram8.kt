de6b66f7827585a16f1901e17ac7800b34960954
jhong pathay <jeffersonpathay@gmail.com>
2022-09-05 19:40:17 +0800

    Kotlin Program 7 1stcom

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,11 @@
+fun main(){
+    //while loop
+    //init
+    var ctr : Int = 1
+    //condition
+    while(ctr <=5){
+        println("Hello World! $ctr")
+        //update
+        ctr++
+    }
+}
