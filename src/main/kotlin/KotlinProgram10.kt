d0f2ead41353534452a4d62e6c30ca1c569141e9
jhong pathay <jeffersonpathay@gmail.com>
2022-09-07 20:02:02 +0800

    Kotlin Program 10 4thcom

--- src/main/kotlin/KotlinProgram10.kt
+++ src/main/kotlin/KotlinProgram10.kt
@@ -1,16 +1,16 @@
 fun main(){
 
-    var ctr1 : Int = 1
-    while(ctr1 <= 5){
-     var ctr2 : Int = 1
-        while(ctr2 <=5){
-            print("@")
-            ctr2++
-        }
-        println()
-        ctr1++
-    }
-    //output
+//    var ctr1 : Int = 1
+//    while(ctr1 <= 5){
+//     var ctr2 : Int = 1
+//        while(ctr2 <=5){
+//            print("@")
+//            ctr2++
+//        }
+//        println()
+//        ctr1++
+//    }
+   // //output
     //@@@@@
     //@@@@@
     //@@@@@
@@ -25,5 +25,24 @@ fun main(){
 //            condition = false
 //        }
 //    }
+
+    var num1 : Int = 0
+    var condition : Boolean = true
+
+
+    while(condition){
+        print("Enter a number:")
+        var inputNumber : Int = readln().toInt()
+        num1 = num1 + inputNumber
+
+
+        println("Do you want to continue?[Yes/No]")
+        var question : String = readln().toString()
+        if (question == "No"){
+            println(num1)
+            condition = false
+        }
+    }
+
 }
 
