5dd952e7f66ec7b51a5bde528d246674825ff24e
jhong pathay <jeffersonpathay@gmail.com>
2022-09-07 08:41:47 +0800

    Kotlin Program 6 2ndcom

--- src/main/kotlin/KotlinProgram6.kt
+++ src/main/kotlin/KotlinProgram6.kt
@@ -4,8 +4,15 @@ fun main() {
 //        println(number)
 //    }
 
-    for(letter in 'B' .. 'P'){
-        println(letter)
-    }
+//    for(letter in 'B' .. 'P'){
+//        println(letter)
+//    }
 
+    var num1 : Int = 0
+    for(number in 1 .. 5){
+        print("Enter a number:")
+        var inputNumber : Int = readln().toInt()
+        num1 = num1 + inputNumber
+    }
+    println(num1)
 }
