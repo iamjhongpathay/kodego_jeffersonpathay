8fb6422ff9dbe655bcb68e06030bf3407a88efef
jhong pathay <jeffersonpathay@gmail.com>
2022-09-07 20:01:04 +0800

    Kotlin Program 13 2ndcom

--- src/main/kotlin/KotlinProgram13.kt
+++ src/main/kotlin/KotlinProgram13.kt
@@ -1,14 +1,24 @@
 fun main(){
     //implicit declaration
     //                     0       1       2
-//    var names = arrayOf("John", "Paul", "Mark")
-//    println(names[1])
+    var names = arrayOf("John", "Paul", "Mark")
+    println(names[1])
 
+    println(names[0])
+    println(names[1])
+    println(names[2])
+    println("********************")
+    names.set(1, "Matthew")
+    println(names[0])
+    println(names[1])
+    println(names[2])
 
+    println("********************")
     //explicit
     var number = arrayOf<Int>(2, 4, 6, 23, 100)
     println(number[4])
 
     //array size
     println(number.size)
+
 }
