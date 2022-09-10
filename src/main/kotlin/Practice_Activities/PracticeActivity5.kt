d798b3b2c3e61ed448578a3b14ec1474f76f7968
jhong pathay <jeffersonpathay@gmail.com>
2022-09-10 16:11:27 +0800

    Practice Activity 5 1stcom

---
+++ src/main/kotlin/Practice_Activities/PracticeActivity5.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+fun main(){
+
+    //Enter 5 digits, store in array, display in correct sequence, display in reverse
+
+    var numbers = arrayOf<Int>(1, 2, 3, 4, 5)
+    var ctr : Int = 0
+    while (ctr < numbers.size) {
+        print("Enter Number ${ctr+1}: ")
+        numbers[ctr] = readln().toInt()
+
+        ctr++
+    }
+    //displaying all number
+    var ctr2 : Int = 0
+    while (ctr2 < numbers.size){
+        print(numbers[ctr2])
+
+        ctr2++
+    }
+    println()
+    println(numbers.reversed())
+
+
+}
