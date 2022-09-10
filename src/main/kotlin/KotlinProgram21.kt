7ff94d1a0b860f6bb414c84bafe165b761d635a9
jhong pathay <jeffersonpathay@gmail.com>
2022-09-10 11:37:47 +0800

    Kotlin Program 21 1stcom

---
+++ src/main/kotlin/KotlinProgram21.kt
@@ -0,0 +1,31 @@
+/* Class -> objects
+objects -> variables
+           functions
+
+ */
+fun main (){
+    //instantiation - process of creating an object
+    var myObject = MyClass("Hello Wortld")
+    var myObjectInt= MyClassInteger(2151)
+
+    var myGenericObject = MyGenericsClass("Hello generic")
+}
+//Non-Generics
+class MyClass(value: String){
+    init{
+        println(value)
+    }
+}
+class MyClassInteger(value: Int){
+    init{
+        println(value.toString().length)
+    }
+}
+
+//Generics
+class MyGenericsClass<T>(value: T){
+    init {
+        println(value.toString().length)
+    }
+}
+
