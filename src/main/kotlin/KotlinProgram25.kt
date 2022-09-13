416359c4d9864a483f1b93f95caa0176ae5d37d3
jhong pathay <jeffersonpathay@gmail.com>
2022-09-13 21:09:19 +0800

    Kotlin Program 25 1stcom

---
+++ src/main/kotlin/KotlinProgram25.kt
@@ -0,0 +1,18 @@
+fun main(){
+    var numbers = intArrayOf(0, 0 , 0)
+
+    for(i in 0 .. 2){
+        println("Enter number ${i+1}")
+        var num = readln().toInt()
+        numbers[i] = num
+    }
+    println(total(*numbers))
+}
+
+fun total(vararg  numbers: Int): Int{
+    var total : Int = 0
+    for(value in numbers){
+        total = total + value
+    }
+    return total
+}
