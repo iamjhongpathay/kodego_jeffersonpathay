e5771e0cb5f90c5aab201cc4d05e804993ea8fc0
jhong pathay <jeffersonpathay@gmail.com>
2022-09-14 21:25:29 +0800

    Kotlin Program 27 1stcom

---
+++ src/main/kotlin/KotlinProgram27.kt
@@ -0,0 +1,30 @@
+fun main (){
+    var joe = Person( "Joe", 21) //instantiation
+    println("**************")
+    println("${joe.name}'s current age is ${joe.name}")
+    println("After 1 year . . . ")
+    joe.ageUp()
+    println("${joe.name}'s current age is ${joe.age}")
+}
+
+class Person(var name: String, var age: Int){
+
+    init{
+        println("Object Created!")
+        println("name is $name")
+        println("age is $age")
+    }
+    fun walk(){
+        println("A person walks")
+    }
+    fun eat(){
+        println("A person eat")
+    }
+    fun displayAge(){
+        println(age)
+    }
+    fun ageUp(){
+
+        age++
+    }
+}
