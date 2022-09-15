b5a1e92d15132318b09d8f11d740bab0db8abff9
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-16 07:59:52 +0800

    Kotlin Program 27 3rdcom

--- src/main/kotlin/KotlinProgram27.kt
+++ src/main/kotlin/KotlinProgram27.kt
@@ -1,19 +1,19 @@
 fun main (){
-    var joe = Person( "Joe", 21) //instantiation
-    println("**************")
-    println("${joe.name}'s current age is ${joe.age}")
-    println("After 1 year . . . ")
-    joe.ageUp()
-    println("${joe.name}'s current age is ${joe.age}")
+    var anne = Person("Anne")
+    anne.age = 25
+    anne.displayAge()
 }
 
-class Person(var name: String, var age: Int){
+class Person(var name: String, var age: Int){ //constructor -> primary constructor
 
     init{
         println("Object Created!")
         println("name is $name")
         println("age is $age")
     }
+    //secondary constructor
+    constructor(name: String):this(name,0){}
+    constructor(age: Int):this("default_name", age){}
     fun walk(){
         println("A person walks")
     }
