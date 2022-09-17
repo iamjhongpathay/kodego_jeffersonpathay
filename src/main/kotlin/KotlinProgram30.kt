97af8fb732dea19242965ab340f9247b800bc47c
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-17 10:41:26 +0800

    Kotlin Program 30 1st commit

---
+++ src/main/kotlin/KotlinProgram30.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+//    var num1 : Int = 2
+//    var num2 : Int =3
+//
+//    var sum: Int = num1 + num2
+//    println("The sum of $num1 and $num2 is $sum")
+
+//    var ctr : Int = 1
+//    while(ctr <= 5){
+//        print(ctr)
+//        ctr++
+//    }
+
+    var ctr : Int = 1
+    while(ctr <= 5){
+        var ctr2 : Int = 1
+        while (ctr2 <= 3){
+            print("@")
+            ctr2++
+        }
+        println()
+        ctr++
+    }
+}
