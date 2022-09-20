5b224caf4d237a4ed3bb82097ee8cf519341bb6d
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-20 21:09:17 +0800

    KotlinProgram31 1st commit

---
+++ src/main/kotlin/KotlinProgram31.kt
@@ -0,0 +1,9 @@
+fun main(){
+    try{
+        println("Enter Number: ")
+        var num : Int = readln().toInt()
+    }catch(e: Exception){
+        println("Invalid! Numbers only")
+    }
+    println("-End of code-")
+}
