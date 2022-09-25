fe5663363944466826388624228a69a60bf1f82e
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-25 19:49:07 +0800

    Practice Activity 10 - 1st commit

--- src/main/kotlin/Practice_Activities/PracticeActivity10.kt
+++ src/main/kotlin/Practice_Activities/PracticeActivity10.kt
@@ -1,62 +1,177 @@
+import ch.qos.logback.core.net.SocketConnector.ExceptionHandler
+import java.beans.ExceptionListener
+
 fun main(){
+    /*
+        Practice Activity 10
 
-    var phone = SmartPhone(15.00, 35)
+        Create a class called SmartPhone
 
-    phone.buyLoad(25.00)
-    phone.chargeBattery(40)
-    phone.checkBalance()
+        attribute(use proper data types):
+        load
+        batteryPercentage
+        (you add your own)
+
+        function:
+        buyLoad(amount) // does not accept 0 or negative values
+        chargeBattery(percent) //does not accept 0 or negative values
+        sendText(message) //display message sent
+        checkBalance() //display current load, if zero, display "Check operator service!"
+        call(minutes) //8 load per minute, will not push tru if no sufficient load
+     */
+
+    //Calling class SmartPhone
+    var durianProMax = SmartPhone(35, 65)
+
+    var condition : Boolean = true
+    while (condition){
+        //Main Menu UI
+        println("[-----Durian Pro Max Smart Phone-----]")
+        println("[1] Buy a Load")
+        println("[2] Charge the Battery")
+        println("[3] Send a Text Messages")
+        println("[4] Check the Load Balance")
+        println("[5] Make a Call")
+        println("[6] Power Off")
+        println()
+        print("[#]Choose a number to interact: ")
+        var userChoices : Int = readln().toInt()
+        if(userChoices == 1){
+            try{
+                println()
+                println("[-----Buy a Load-----]")
+                print("[#]Enter the Amount of load you want to buy: ")
+                durianProMax.buyLoad(readln().toInt())
+            }catch (e: NumberFormatException){
+                println("[!]Error: Input Digits Only!")
+                println()
+            }
+
+        }else if(userChoices == 2){
+            try {
+                println()
+                println("[-----Charge the Battery-----]")
+                println("The battery of Durian Pro Max is ${durianProMax.batteryPercentage}%")
+                print("[?]How much of battery percent do you want to add? ")
+                durianProMax.chargeBattery(readln().toInt())
+            }catch (e: NumberFormatException){
+                println("[!]Error: Input Digits Only!")
+                println()
+            }
+        }else if(userChoices == 3) {
+            try {
+                println("[-----Send a Text Messages-----]")
+                println()
+                print("Contact Number of Recipient: ")
+                var recipientNum : Long = readln().toLong()
+                print("Text Message: ")
+                var message: String = readln()
+                durianProMax.sendText(recipientNum, message)
+                println()
+            } catch (e: NumberFormatException){
+                println("[!]Error: Input Digits Only! The contact number of recipient is invalid.")
+                println()
+            }
+        }else if(userChoices == 4){
+            println("[-----Check the Load Balance-----]")
+            println()
+            durianProMax.checkBalance()
+        }else if(userChoices == 5){
+            try{
+                println("[-----Make a Call-----]")
+                println()
+                print("[#]Contact Number of Recipient: ")
+                var recipientNum : Long = readln().toLong()
+                print("[?]Do you want to call this number $recipientNum?[Yes/No]: ")
+                var userOption : String = readln().lowercase()
+                if (userOption == "yes"){
+                    print("[?]How long do you want to make a call per minute? ")
+                    var minutes : Int = readln().toInt()
+                    durianProMax.call(recipientNum, minutes)
+                }else if(userOption == "no"){
+                    println("[!]Call Cancelled!")
+                    println()
+                }
+            }catch (e: NumberFormatException){
+                println("[!]Error: Input Digits Only!")
+            }
+        }else if(userChoices == 6){
+            println()
+            println("Thank You! Goodbye!")
+            println("")
+            condition = false
+        }
+    }
 }
 
-class SmartPhone(var spLoad: Double, var spBatteryPercentage : Int){
+class SmartPhone(var load : Int, var batteryPercentage : Int){
 
-    //buyLoad
-    var load = spLoad
-        get() = field
-        set(value){
-            if(value < 0){
-                println("There is no load below 0.00")
-            }else{
-                field = value
-                println("Hi! You have successfully buy a regular load. Your current load is $load Pesos Load.")
-            }
-        }
-
-    //chargeBattery
-    var batteryPercent = spBatteryPercentage
-        get() = field
-        set(value){
-            if(value < 0){
-                println("Invalid value! The Battery Percentage should not subceed from 1%")
-            }else if (value > 100){
-                println("Invalid value! The total percentage of the Battery should not exceed to 100%. Are you Taguro?")
-            }else{
-                field = value
-                println("Battery Charging with $batteryPercent%")
-            }
-        }
-
-    //instantiation
     init{
-        println("Smart Phone Object Created!")
-        println("Initial value of load is $spLoad Pesos and batteryPercentage is $spBatteryPercentage%")
+        println("Initial value of your load is $load Pesos and the batteryPercentage is $batteryPercentage %")
         println()
     }
 
-    //functions
-    //sum of load
-    fun buyLoad(amount: Double){
-        this.load = this.load + amount
+    fun buyLoad(amount: Int){                                                                       //[1] Buy a Load
+            if(amount < 0){
+                println("[!]Error: Invalid Amount! The amount should be greater than zero(0).")
+                println()
+            }else{
+                load += amount
+                println("[!]Message: Successful! You bought a load in a amount of $amount Pesos.")
+                println()
+            }
     }
-    //sum of charge battery percentage
-    fun chargeBattery(chargingPercent: Int){
-        this.batteryPercent = this.batteryPercent + chargingPercent
+
+    fun chargeBattery(percent: Int){                                                                 //[2] Charge the Battery
+        var batteryTotal : Int = 0
+        batteryTotal = percent + batteryPercentage
+            if(percent < 0){
+                println("[!]Error: The amount of battery percentage should be greater than zero(0).")
+                println()
+            }else if(batteryTotal > 100){
+                println("[!]Error: The battery is OVER CHARGED!")
+                println()
+            }else{
+                batteryPercentage += percent
+                println("[!]Message: Successful! The battery is increased by $percent%")
+                println()
+            }
     }
-    //Balance Checking
-    fun checkBalance(){
-        if(this.load < 1){
-            println("0.00 Peso Balance. Check Operator Services")
+
+    fun sendText(recipientNum: Long, message : String){                                              //[3] Send a Text Messages
+        if(load == 0){
+            println("[!]Error: Your load is INSUFFICIENT! Check Operator Service")
+            println()
         }else{
-            println("Your current load balance is $load Pesos")
+
+            println("[!]Message Sent Successfully!")
+            println()
+            println("Sent to: $recipientNum")
+            println("Message: $message")
+            println("[!]Load Balance: ${--load} Pesos")
+        }
+    }
+
+    fun checkBalance(){                                                                              //[4] Check the Load Balance
+        if (load == 0){
+            println("[!]Error: Your load is INSUFFICIENT! Check Operator Service")
+            println()
+        }
+        println("[!]Message Received! Your current load balance is $load Pesos")
+        println()
+    }
+
+    fun call(recipientNum: Long, minutes: Int){                                                     //[5] Make a Call
+        var sumCalls : Int = minutes * 8
+        if(sumCalls > load){
+            println("[!]Sorry: Your load is INSUFFICIENT! Check Operator Service")
+            println()
+        }else{
+            load -= (minutes * 8)
+            println()
+            println("[!]You Make a Call Successfully!")
+            println("The call is lasted $minutes minutes with $recipientNum.")
+            println()
         }
     }
 
