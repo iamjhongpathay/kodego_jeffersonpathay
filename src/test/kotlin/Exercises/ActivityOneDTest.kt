675bd271946db81b26ddf1c9201d5002e60b7708
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-25 22:50:37 +0800

    ActivityOneDTest.kt for Activity03_B.kt - 1st commit

--- src/test/kotlin/Exercises/ActivityOneDTest.kt
+++ src/test/kotlin/Exercises/ActivityOneDTest.kt
@@ -1,5 +1,24 @@
 package Exercises
 
+import ActivityOneD
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class ActivityOneDTest
+//This Test is for Activity03_B.kt
+
+class ActivityOneDTest{
+
+    @Test
+    fun twoPlusTwoEqualsFour(){
+        var activityOneD = ActivityOneD(2)
+        assertEquals(4, activityOneD.addAmount(2))
+    }
+
+    //35 divided by 5 = 7
+    @Test
+    fun ThirtyFiveDividedByFiveEqualsSeven(){
+        var activityOneD = ActivityOneD(35)
+        assertEquals(7, activityOneD.dividedBy(5))
+    }
+
+}
