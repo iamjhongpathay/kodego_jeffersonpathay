dcfd0416542f86395c0a73787af0688edc596ad4
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-30 11:30:07 +0800

    Products_A03DTest - 1st commit

--- src/test/kotlin/Exercises/Products_A03DTest.kt
+++ src/test/kotlin/Exercises/Products_A03DTest.kt
@@ -1,5 +1,31 @@
 package Exercises
 
+import Products_A03D
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class Products_A03DTest
+class Products_A03DTest{
+
+  //If pCode is same with productCode return false
+//  @Test
+//  fun pCodeIsSameWithProductCodeReturnFalse(){
+//   var productsA03d = Products_A03D()
+//   assertFalse(productsA03d.addProduct("userChose", "pCategory", "PNB350G", "pName",
+//       "pDescription", 0, 0.0, "PNB350G")); false
+//  }
+
+    //All the parameters is not empty return True
+    @Test
+    fun allTheParametersAreNotEmptyReturnTrue(){
+        var productsA03d = Products_A03D()
+        assertTrue(productsA03d.addProduct("userChose", "pCategory", "pCode", "pName",
+            "pDescription", 0, 0.0, "productCode")); true
+    }
+
+    //Product Details is empty or Quantities and Price is less than 0 return false
+    @Test fun ProductDetailsIsEmptyOrQuantityAndPriceIsLessThanZeroReturnFalse(){
+    var productsA03d = Products_A03D()
+    assertFalse(productsA03d.addProduct("userChose", "", "", "",
+        "", -1, -1.0, "productCode")); false
+    }
+ }
