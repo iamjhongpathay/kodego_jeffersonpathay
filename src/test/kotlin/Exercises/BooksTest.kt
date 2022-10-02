51396231a05211ae41b39e89af5038624cc92200
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-10-02 16:07:13 +0800

    BooksTest - 1st commit

--- src/test/kotlin/Exercises/BooksTest.kt
+++ src/test/kotlin/Exercises/BooksTest.kt
@@ -1,5 +1,23 @@
 package Exercises
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class BooksTest
+class BooksTest(){
+
+    //details to borrow a book is empty return false
+    @Test
+    fun detailsToBorrowABookIsEmptyReturnFalse(){
+        var books = Books()
+        assertFalse(books.borrowBook(0,"", "", 0,
+            "", 0, "")); false
+    }
+
+    //details to borrow a book have value return true
+    @Test
+    fun detailsToBorrowABookHaveValueReturnFalse(){
+        var books = Books()
+        assertTrue(books.borrowBook(123,"02/10/2022", "02/15/2022", 321,
+            "Angel", 456, "Pedro")); true
+    }
+}
